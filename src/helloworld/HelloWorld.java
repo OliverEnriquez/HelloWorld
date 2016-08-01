cffc42372e461dba2120d4abe60149e919a37afb
odec12 <odec12@gmail.com>
2016-08-01 12:06:22 -0600

    Hola Otra Vez

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
     }
     
     public static void HelloWorldPrint(){
-            System.out.println("Hello world");
+            System.out.println("Holaaaa OtraVez");
     }
     
 }
